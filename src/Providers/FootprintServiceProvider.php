521bbab27e4bdd412af8b26d4c9cb8228fe100a2
Justice Sandram <justicesandram@gmail.com>
2026-01-10 09:57:03 +0200

    feat: implement migration file renaming on vendor:publish command completion

--- src/Providers/FootprintServiceProvider.php
+++ src/Providers/FootprintServiceProvider.php
@@ -4,6 +4,8 @@ namespace TNM\Footprints\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Routing\Router;
+use Illuminate\Console\Events\CommandFinished;
+use Illuminate\Support\Facades\Event;
 use TNM\Footprints\Http\Middleware\CaptureFootprintsMiddleware;
 
 class FootprintServiceProvider extends ServiceProvider
@@ -15,8 +17,26 @@ class FootprintServiceProvider extends ServiceProvider
         ], 'footprints-config');
 
         $this->publishes([
-            __DIR__ . '/../../database/migrations' => database_path('migrations'),
+            __DIR__ . '/../../database/migrations/create_footprints_table.php' => database_path('migrations/create_footprints_table.php'),
         ], 'footprints-migrations');
+
+        if ($this->app->runningInConsole()) {
+            Event::listen(CommandFinished::class, function (CommandFinished $event) {
+                if ($event->command === 'vendor:publish') {
+                    $tags = $event->input->getOption('tag') ?: [];
+                    if (empty($tags) || in_array('footprints-migrations', $tags)) {
+                        $migrationsPath = database_path('migrations');
+                        $oldFile = $migrationsPath . '/create_footprints_table.php';
+                        
+                        if (file_exists($oldFile)) {
+                            $timestamp = date('Y_m_d_His');
+                            $newFile = $migrationsPath . '/' . $timestamp . '_create_footprints_table.php';
+                            rename($oldFile, $newFile);
+                        }
+                    }
+                }
+            });
+        }
 
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
 
