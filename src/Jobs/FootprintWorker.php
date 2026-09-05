7efcb73d6fcc4e12ff4f16870c6adf3354878420
Justice Sandram <108130114+justicesandram@users.noreply.github.com>
2026-09-05 06:55:46 +0200

    feat: enhance footprint logging with Kafka integration and improved middleware
    - Updated the package description to reflect Kafka topic publishing.
    - Added `ext-rdkafka` as a requirement for Kafka support.
    - Implemented `FootprintMiddleware` for capturing and processing request/response data.
    - Introduced `FootprintWorker` for asynchronous logging to Kafka with error handling.
    - Added configuration options for Kafka in `footprints.php`, including brokers and security settings.
    - Enhanced utility functions for validating Kafka configurations and checking broker connectivity.

--- src/Jobs/FootprintWorker.php
+++ src/Jobs/FootprintWorker.php
@@ -2,7 +2,75 @@
 
 namespace TNM\Footprints\Jobs;
 
-class FootprintWorker
-{
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Throwable;
+use TNM\Footprints\Utils\KafkaLog;
 
+class FootprintWorker implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(public array $footprint)
+    {
+
+    }
+
+    public function fail(Throwable $exception = null): void
+    {
+        $this->logToDatabase($exception->getMessage());
+    }
+
+    public function handle(): void
+    {
+        try {
+            $kafka = new KafkaLog();
+
+            $kafka->log($this->footprint);
+        } catch (Throwable $e) {
+            Log::error("Failed to handle footprint job: " . $e->getMessage());
+            $this->logToDatabase($e->getMessage());
+        }
+    }
+
+    private function logToDatabase(string $error): void
+    {     Log::debug("Logging footprint to database now...");
+
+        DB::connection()->table(config("footprints.table_name"))->insert([
+            'request_id' => $this->footprint['request_id'],
+            'app_name' => $this->footprint['app_name'],
+            'app_environment' => $this->footprint['app_environment'],
+
+            'request_method' => $this->footprint['request_method'],
+            'request_uri' => $this->footprint['request_uri'],
+            'request_url' => $this->footprint['request_url'],
+            'request_time' => $this->footprint['request_time'],
+
+            'request_headers' => $this->footprint['request_headers'],
+            'request_body' => $this->footprint['request_body'],
+
+            'response_status_code' => $this->footprint['response_status_code'],
+            'response_success' => $this->footprint['response_success'],
+
+            'response_headers' => $this->footprint['response_headers'],
+            'response_body' => $this->footprint['response_body'],
+
+            'duration_ms' => $this->footprint['duration_ms'],
+
+            'client_ip' => $this->footprint['client_ip'],
+            'host_ip' => $this->footprint['host_ip'],
+            'host_name' => $this->footprint['host_name'],
+
+
+            'user_id' => $this->footprint['user_id'],
+            'user_type' => $this->footprint['user_type'],
+
+            'exception_message' => $this->footprint['exception_message'] ?? $error,
+        ]);
+    }
 }
--- src/config/footprints.php
+++ src/config/footprints.php
@@ -1,1 +1,27 @@
 <?php
+
+return [
+    'enabled' => env('FOOTPRINTS_ENABLED', true),
+    'app_name' => env('FOOTPRINTS_APPLICATION_NAME', env('APP_NAME', 'unnamed-laravel-app')),
+    'queue' => [
+        'connection' => env('FOOTPRINTS_QUEUE_CONNECTION', 'database'),
+        'queue' => env('FOOTPRINTS_QUEUE_NAME', 'default'),
+    ],
+    'mask_fields' => env(
+        "FOOTPRINTS_HIDDEN_FIELDS",
+        "password,password_confirmation,new_pin,pin,credit_card,api_key,token"
+    ),
+    "table_name" => env('FOOTPRINTS_TABLE_NAME', 'application_footprints'),
+    'kafka' => [
+        'brokers' => env('FOOTPRINTS_KAFKA_BROKERS'),
+        'topic' => env('FOOTPRINTS_KAFKA_TOPIC', 'application_footprints'),
+        'client_id' => env('FOOTPRINTS_KAFKA_CLIENT_ID', 'laravel_logger'),
+        'timeout_ms' => env('FOOTPRINTS_KAFKA_TIMEOUT_MS', 1000),
+        'sasl_mechanism' => env('FOOTPRINTS_KAFKA_SASL_MECHANISM', null),
+        'security_protocol' => env('FOOTPRINTS_KAFKA_SECURITY_PROTOCOL', null),
+        'sasl_username' => env('FOOTPRINTS_KAFKA_SASL_USERNAME', null),
+        'sasl_password' => env('FOOTPRINTS_KAFKA_SASL_PASSWORD', null),
+        // e.g., 'message_key_func' => function($footprint) { return $footprint['request_id']; }
+        'message_key_func' => env('FOOTPRINTS_KAFKA_MESSAGE_KEY_FUNC', "TNM\\Utils\\getDefaultEventKey")(...),
+    ]
+];
